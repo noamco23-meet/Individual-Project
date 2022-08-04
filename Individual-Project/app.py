14894474590988129705b9cfa312999c439cfb74
Noam Cohen-Ofir <noamcohenofir@gmail.com>
2022-08-04 14:33:15 +0300

    probably maybe finished
    hooray (?)

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -3,6 +3,8 @@ from flask import session as login_session
 import pyrebase
 from collections import OrderedDict
 import json
+import datetime
+
 config = {
   "apiKey": "AIzaSyBwW3x-5gkCMy3bhaeOFXkcgaUGJmpGjyw",
   "authDomain": "personal-project-51039.firebaseapp.com",
@@ -12,7 +14,7 @@ config = {
   "appId": "1:573878428747:web:2aebcb6dd0a166882759ce",
   "measurementId": "G-X2BSVG1JHX",
   "databaseURL":"https://personal-project-51039-default-rtdb.europe-west1.firebasedatabase.app/"
-}
+}               
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
@@ -22,6 +24,20 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 GROUPS = ["Group A", "Group B", "Group C", "Group D", "Group E", "Group F"]
+
+
+
+def find_user_through_email(email):
+    for user in db.child("Users").get().val():
+
+            user_dict = json.loads(json.dumps(db.child("Users").child(user).get().val()))
+
+            if user_dict["email"] == email:
+                second_user_localId = user
+                second_user_name = user_dict['name']
+                final_second_user_dict = user_dict
+    return [second_user_localId, second_user_name, final_second_user_dict]
+
 
 @app.route("/", methods=['GET', 'POST'])
 def login():
@@ -65,7 +81,8 @@ def signup():
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
-    return render_template("home.html", user=db.child("Users").child(login_session['user']['localId']).get().val())
+    user = db.child("Users").child(login_session['user']['localId']).get().val()
+    return render_template("home.html", user=user, action=list(user['history'].values())[0])
 
 
 
@@ -76,22 +93,23 @@ def transfer():
         other_email = request.form['other_email']
         amount = int(request.form['amount'])
 
-        for user in db.child("Users").get().val():
+        user_info = find_user_through_email(other_email)
+        second_user_localId = user_info[0]
+        second_user_name = user_info[1]
+        final_second_user_dict = user_info[2]
 
-            user_dict = json.loads(json.dumps(db.child("Users").child(user).get().val()))
-
-            if user_dict["email"] == other_email:
-                second_user_localId = user
-                second_user_name = user_dict['name']
-                final_second_user_dict = user_dict
         try:
             first_user_new_balance={'balance' : db.child("Users").child(login_session['user']['localId']).get().val()['balance'] - amount}
             db.child("Users").child(login_session['user']['localId']).update(first_user_new_balance)
             second_user_new_balance = {'balance':((final_second_user_dict['balance']) + amount)}
             db.child("Users").child(second_user_localId).update(second_user_new_balance)
 
-            new_history_first_user = {f"Payed ${amount} to {second_user_name}": "Payment"}
-            new_history_second_user = {f"Received ${amount} from {second_user_name}": "#baller"}
+            x = datetime.datetime.now()
+            x = x.strftime("%A %d %H:%I")
+
+            new_history_first_user = {f"Payed {amount} to {second_user_name} at {x}": "Payment"}
+            user_name = db.child("Users").child(login_session['user']['localId']).child('name').get().val()
+            new_history_second_user = {f"Received {amount} from {user_name} at {x}": "#baller"}
             db.child("Users").child(login_session['user']['localId']).child('history').update(new_history_first_user)
             db.child("Users").child(second_user_localId).child('history').update(new_history_second_user)
 
@@ -109,8 +127,32 @@ def transfer():
 
 @app.route('/history')
 def history():
-    print(json.loads(json.dumps(db.child("Users").child(login_session['user']['localId']).get().val())))
     return render_template("history.html", history_list = json.loads(json.dumps(db.child("Users").child(login_session['user']['localId']).get().val()))['history'])
+
+# @app.route('/leaderboard')
+# def leaderboard():
+#     money_board = []
+#     for user in db.child("Users").get().val():
+#         user_dict = json.loads(json.dumps(db.child("Users").child(user).get().val()))
+#         money_board.append(user_dict['balance'])
+#         money_board = sorted(money_board)
+    
+#     user_order = []
+#     for i in range(len(money_board)-1,0,-1):
+#         print(money_board[i])
+#         for user in db.child("Users").get().val():
+#             user_dict = json.loads(json.dumps(db.child("Users").child(user).get().val()))
+#             if (user_dict['balance']) == money_board[i]:
+#                 user_info = find_user_through_email(user_dict['email'])
+#                 user_order.append(user_info[0])
+#                 print(user_dict)
+
+   
+#     return render_template("leaderboard.html", user_order=user_order, users=db.child("Users"))
+
+@app.route('/user_hall')
+def user_hall():
+    return render_template("user_hall.html", users=json.loads(json.dumps(db.child("Users").get().val())))
 
 
 @app.route('/logout')
@@ -120,6 +162,10 @@ def logout():
     return redirect(url_for('login'))
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
 
 
 #add dialog box to confirm money transfer
